0c028c21e73509b0bafe537bcc0f88b081eefd37
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2020-09-29 20:48:39 -0400

    Turns out, we didn't need jQuery

--- src/database/googleSheetsDatabase.tsx
+++ src/database/googleSheetsDatabase.tsx
@@ -1,21 +1,12 @@
 import { parse } from 'papaparse';
-import $ from 'jquery';
-
-const directoryKey = '2PACX-1vRUfbGxSFyI7zQs0e7n-ZckeJH3_e7niFh2SLcHFT7Z7lM_82R4Z8lbQVRNU85EKXtpJ7f-WkYT4ZFt';
 
 export const getAllLineItems = new Promise(function(resolve, reject) {
-  $(document).ready(function () {
-    $.ajax({
-      type: "GET",
-      url: 'https://cors-anywhere-cfb-refunder.herokuapp.com' + '/https://docs.google.com/spreadsheets/d/e/2PACX-1vRUfbGxSFyI7zQs0e7n-ZckeJH3_e7niFh2SLcHFT7Z7lM_82R4Z8lbQVRNU85EKXtpJ7f-WkYT4ZFt/pub?output=csv',
-      dataType: "text",
-      success: 
-            function(results) {
-              console.log('JQuery results: ' + results)
-              var data = results.data
-              resolve(data);
-              console.log('JQuery data: ' + data)
-            }
+    parse('https://cors-anywhere-cfb-refunder.herokuapp.com' + '/https://docs.google.com/spreadsheets/d/e/2PACX-1vRUfbGxSFyI7zQs0e7n-ZckeJH3_e7niFh2SLcHFT7Z7lM_82R4Z8lbQVRNU85EKXtpJ7f-WkYT4ZFt/pub?output=csv', {
+              download: true,
+              header: true,
+              complete: function(results) {
+                var data = results.data
+                resolve(data);
+              }
     });
-  });
-});
+}); 
