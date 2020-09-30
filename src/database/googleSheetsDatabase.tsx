b86ed0ae4c48c41e4082a01391cf3cc0a150612f
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2020-09-29 22:59:02 -0400

    Made it easy to switch to cfb sheet when published

--- src/database/googleSheetsDatabase.tsx
+++ src/database/googleSheetsDatabase.tsx
@@ -1,7 +1,10 @@
 import { parse } from 'papaparse';
 
+const key = '1uU_XuPUYlJbjhW13rBK_gOVdgc32u6iv-PbkJvZ-g3g';
+const personal_key = '2PACX-1vRUfbGxSFyI7zQs0e7n-ZckeJH3_e7niFh2SLcHFT7Z7lM_82R4Z8lbQVRNU85EKXtpJ7f-WkYT4ZFt';
+
 export const getAllLineItems = new Promise(function(resolve, reject) {
-    parse('https://cors-anywhere-cfb-refunder.herokuapp.com' + '/https://docs.google.com/spreadsheets/d/e/2PACX-1vRUfbGxSFyI7zQs0e7n-ZckeJH3_e7niFh2SLcHFT7Z7lM_82R4Z8lbQVRNU85EKXtpJ7f-WkYT4ZFt/pub?output=csv', {
+    parse('https://cors-anywhere-cfb-refunder.herokuapp.com/' + 'https://docs.google.com/spreadsheets/d/e/' + personal_key + '/pub?output=csv', {
               download: true,
               header: true,
               complete: function(results) {
