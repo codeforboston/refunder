b2d296a516bbbdc6d21649b941a7eff660168c63
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2020-11-10 21:19:43 -0500

    Tried to make d3 and React work better.
    From https://spin.atomicobject.com/2017/07/20/d3-react-typescript/

--- src/components/visualization/BarChart.tsx
+++ src/components/visualization/BarChart.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
+import { isConstructorDeclaration } from "typescript";
+import { render } from "@testing-library/react";
 
 interface BarChartProps {
   width: number;
@@ -17,25 +19,42 @@ export class BarCharts extends Component<BarChartProps, {}> {
   }
 
   componentDidMount() {
-    var max_all = Math.max(...this.props.values);
-    var bar_scale_height = d3.scaleLinear().domain([0, max_all]).range([0, this.props.height]);
-    var bar_scale_place = d3.scaleLinear().domain([0, max_all]).range([this.props.height, 0]);
-
-    let bar_width = this.props.width / (this.props.values.length + 1);
     console.log(this.props.labels);
     console.log(this.props.values);
 
-    var u = d3.select(this.myRef.current)
-        .selectAll('rect')
-        .data(this.props.values);
+  }
 
-    u.enter().append('rect')
-        .attr('width', bar_width - 1)
-        .attr('height', (d) => bar_scale_height(d) + 'px')
-        .attr('x', (d, i) => i * bar_width + 3)
-        .attr('y', (d, i) => bar_scale_place(d) + 'px')
-        .attr('fill', 'white');
-    u.exit().remove();
+  render() {
+    let bar_width = this.props.width / (this.props.values.length + 1);
+    return (
+      <svg className="container" ref={ this.myRef } 
+           width = {this.props.width} height={ this.props.height }>
+        <Bars bar_width={bar_width} max_bar_height={this.props.height} labels={this.props.labels}
+              values={this.props.values}/>
+      </svg>
+    )
+  }
+};
+
+interface BarsProps {
+  bar_width: number;
+  max_bar_height: number;
+  labels: string[];
+  values: number[];
+}
+
+class Bars extends Component<BarsProps, {}> {
+  myRef: React.RefObject<SVGSVGElement>;
+
+  constructor(props: BarsProps) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  componentDidMount() {
+    var max_all = Math.max(...this.props.values);
+    var bar_scale_height = d3.scaleLinear().domain([0, max_all]).range([0, this.props.max_bar_height]);
+    var bar_scale_place = d3.scaleLinear().domain([0, max_all]).range([this.props.max_bar_height, 0]);
 
     var t = d3.select(this.myRef.current)
         .selectAll('text')
@@ -43,8 +62,8 @@ export class BarCharts extends Component<BarChartProps, {}> {
 
     t.enter().append('text')
         .attr('fill', 'red')
-        .attr('y', (d, i) => this.props.height - (i % 2) * 28)
-        .attr('x', (d, i) => i * bar_width + 3 + bar_width / 2) 
+        .attr('y', (d, i) => this.props.max_bar_height - (i % 2) * 28)
+        .attr('x', (d, i) => i * this.props.bar_width + 3 + this.props.bar_width / 2) 
         .attr('text-anchor', 'middle')
         .attr('font-size', '14px')
         .text((d) => d);
@@ -52,11 +71,45 @@ export class BarCharts extends Component<BarChartProps, {}> {
   }
 
   render() {
+    var max_all = Math.max(...this.props.values);
+    var bar_scale_place = d3.scaleLinear().domain([0, max_all]).range([this.props.max_bar_height, 0]);
+    const bars = this.props.values.map((val: number, index: number) => {
+      return <Bar key={index} width={this.props.bar_width - 1}
+                  height={val + 'px'} x={index * this.props.bar_width + 3}
+                  y={bar_scale_place(val) + 'px'} />
+    });
+
     return (
-      <svg className="container" ref={ this.myRef } 
-           width = {this.props.width} height={ this.props.height }>
-      </svg>
-    )
+    <g className="bars" ref={this.myRef}>
+      {bars}
+    </g>
+    );
+  }
+};
+
+interface BarProps {
+  width: number;
+  height: string;
+  x: number;
+  y: string;
+}
+
+class Bar extends Component<BarProps, {}> {
+  myRef: React.RefObject<SVGRectElement>;
+
+  constructor(props: BarProps) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  componentDidMount() {
+    d3.select(this.myRef.current).data([this.props]);
+  }
+
+  render() {
+    const {width, height, x, y} = this.props;
+    return <rect className="bar" ref={this.myRef} 
+            width={width} height={height} x={x} y={y} fill={"navy"}/>;
   }
 
 };
